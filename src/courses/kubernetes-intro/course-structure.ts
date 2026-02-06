3395fc5d6a1cc35408dc2905d8d7efb8a06c85c1
Antoine-Flo <antoine.flouzat@gmail.com>
2026-02-06 13:43:24 +0100

    Remove kubernetes objects from intro

--- src/courses/kubernetes-intro/course-structure.ts
+++ src/courses/kubernetes-intro/course-structure.ts
@@ -4,7 +4,6 @@ export const courseStructure: CourseStructure = {
     chapters: [
         { moduleId: 'overview', chapterId: 'onboarding' },
         { moduleId: 'overview', chapterId: 'concepts-fondamentaux' },
-        { moduleId: 'overview', chapterId: 'objets-kubernetes' },
         { moduleId: 'workloads', chapterId: 'pods-intro' },
         { moduleId: 'workloads', chapterId: 'deployments-intro' },
         { moduleId: 'services-networking', chapterId: 'services-intro' },
